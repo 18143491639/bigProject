1e2d50609cdbc168e1ff1e390b604a707b6ca17d
xibJane <1099326218@qq.com>
2020-08-03 16:46:51 +0800

    用户功能结束

--- assets/js/login.js
+++ assets/js/login.js
@@ -16,56 +16,76 @@ $(function () {
 
     form.verify({
         pwd: [/^[\S]{6,16}$/, '密码必须6到16位，且不能出现空格'],
-        repwd: function(value) {
+        repwd: function (value) {
             // 通过形参拿到的是确认密码框中的内容
             // 还需要拿到密码框中的内容
             // 然后进行一次等于的判断
             // 如果判断失败,则return一个提示消息即可
             var pwd = $('.reg-box [name=password]').val()
             if (pwd !== value) {
-              return '两次密码不一致！'
+                return '两次密码不一致！'
             }
-          }
+        }
     })
 
-    $('#form_reg').on('submit', function (e) {  
+    $('#form_reg').on('submit', function (e) {
         e.preventDefault()
         $.ajax({
-            type:'POST',
-            url:'/api/reguser',
+            type: 'POST',
+            url: '/api/reguser',
             data: {
                 username: $('#form_reg [name=username]').val(),
-                password:$('#form_reg [name=password]').val(),
+                password: $('#form_reg [name=password]').val(),
             },
-            success:function(res){
+            success: function (res) {
                 if (res.status !== 0) {
                     return layer.msg(res.message)
                 }
                 layer.msg('注册成功，请登录！')
-                setTimeout(function() {
+                setTimeout(function () {
                     $('#link_login').click()
-                },500)
+                }, 500)
                 $('#form_reg')[0].reset()
             }
         })
     })
 
-    $('#form_login').on('submit', function (e) {  
+    $('#form_login').on('submit', function (e) {
         e.preventDefault()
         $.ajax({
-            type:'POST',
-            url:'/api/login',
+            type: 'POST',
+            url: '/api/login',
             data: $(this).serialize(),
-            success:function(res){
+            success: function (res) {
                 if (res.status !== 0) {
                     return layer.msg(res.message)
                 }
                 layer.msg(res.message)
-                localStorage.setItem('token',res.token)
-                setTimeout(function() {
+                localStorage.setItem('token', res.token)
+                setTimeout(function () {
                     location.href = '/index.html'
-                },500) 
+                }, 500)
             }
         })
     })
+
+    // 定义美化时间的过滤器
+    template.defaults.imports.dataFormat = function (date) {
+        const dt = new Date(date)
+
+        var y = dt.getFullYear()
+        var m = padZero(dt.getMonth() + 1)
+        var d = padZero(dt.getDate())
+
+        var hh = padZero(dt.getHours())
+        var mm = padZero(dt.getMinutes())
+        var ss = padZero(dt.getSeconds())
+
+        return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
+    }
+
+    // 定义补零的函数
+    function padZero(n) {
+        return n > 9 ? n : '0' + n
+    }
 })
